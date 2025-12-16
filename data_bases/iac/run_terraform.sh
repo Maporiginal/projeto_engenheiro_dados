11274a645fda27d226a1ffd4597196d02d130163
Matheus Andrade de Paula <map.mg1@gmail.com>
2025-12-16 18:58:37 -0300

    modificação run_terraform.sh

--- data_bases/iac/run_terraform.sh
+++ data_bases/iac/run_terraform.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-terraform init
-terraform fmt -recursive
-terraform validate
-terraform plan
-terraform apply
+docker exec -it terraform sh -lc 'cd /iac && terraform init'
+docker exec -it terraform sh -lc 'cd /iac && terraform fmt -recursive'
+docker exec -it terraform sh -lc 'cd /iac && terraform validate'
+docker exec -it terraform sh -lc 'cd /iac && terraform plan'
+docker exec -it terraform sh -lc 'cd /iac && terraform apply'
