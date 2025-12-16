e92e853da8ca8f36ceac2cfad6718eb917f3b4f8
Matheus Andrade de Paula <map.mg1@gmail.com>
2025-12-16 16:13:21 -0300

    terraform dentro de container

---
+++ data_bases/iac/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.19"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_glue_catalog_database" "metadata" {
+  name        = "metadata-database-raw-curated"
+  description = "Data Catalog para RAW/CURATED do lab"
+}
